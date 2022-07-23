8ec25599e742e3d3365a22b52c336e820281de42
MShizik <sidorovstasd7@gmail.com>
2022-07-23 13:42:48 +0300

    InviteList

--- app/src/main/java/com/example/life_manager/MainActivity.kt
+++ app/src/main/java/com/example/life_manager/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.life_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var etEmailUser: EditText? = null
+    private var etPasswordUser: EditText? = null
+    private var tvInfoApp: TextView? = null
+
+    private var btnSignUser: Button? = null
+    private var btnRegUser: Button? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etEmailUser = findViewById(R.id.main_text_email_field)
+        etPasswordUser = findViewById(R.id.main_text_password_field)
+        tvInfoApp = findViewById(R.id.main_text_greeting)
+
+        btnSignUser = findViewById(R.id.main_sign_btn)
+        btnRegUser = findViewById(R.id.main_reg_btn)
+
+
+
     }
 }
+
